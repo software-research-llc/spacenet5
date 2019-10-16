d4ba23ab26f2e3a760fda8759706c2178f7f0791
Murray Miron <mmiron@srllc.org>
2019-10-16 16:14:30 -0400

    Update for fixed Sequence class

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import snflow as flow
 import plac
 import tensorflow as tf
 import segmentation_models as sm
-import keras
+import tensorflow.keras as keras
 import numpy as np
 import logging
 logger = logging.getLogger(__name__)
@@ -13,11 +13,12 @@ callbacks = [
     keras.callbacks.ModelCheckpoint('./best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
 ]
 
-dice_loss = sm.losses.DiceLoss(class_weights=np.array([0.2, 1, 1, 1]))
+dice_loss = sm.losses.DiceLoss()#class_weights=np.array([1, 1, 1, 1]))
 focal_loss = sm.losses.BinaryFocalLoss() if flow.N_CLASSES == 1 else sm.losses.CategoricalFocalLoss()
 total_loss = dice_loss + (1 * focal_loss)
-metrics = ['accuracy', sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+metrics = ['accuracy', 'sparse_categorical_crossentropy', sm.metrics.IOUScore(), sm.metrics.FScore()]
 optim = keras.optimizers.Adam()
+preprocess_input = sm.get_preprocessing(flow.BACKBONE)
 
 def save_model(model, save_path="model.hdf5", pause=0):
     if pause > 0:
@@ -37,14 +38,14 @@ def load_weights(model, save_path="model.hdf5"):
         sys.stderr.write(str(exc) + "\n")
 
 def build_model():
-    return sm.Unet(flow.BACKBONE, classes=flow.N_CLASSES, activation='softmax')
+    return sm.Unet(flow.BACKBONE, classes=flow.N_CLASSES, activation='softmax', encoder_weights='imagenet')
 
 def main(save_path="model.hdf5",
          optimizer='adam',
          loss='sparse_categorical_crossentropy',
          restore=True,
          verbose=1,
-         epochs=20,
+         epochs=50,
          validation_split=0.1):
     model = build_model()
     if restore:
@@ -52,15 +53,29 @@ def main(save_path="model.hdf5",
 
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
-    train_seq = flow.Sequence(batch_size=3, transform=0.25, test=False)
-    val_seq = flow.Sequence(batch_size=3, test=True)
+    train_seq = flow.Sequence(batch_size=3, transform=0.00, test=False)
+    val_seq = flow.Sequence(batch_size=1, test=True)
 
-    for i in range(epochs):
-        try:
-            train_step(model, train_seq, verbose, 1, callbacks, save_path, val_seq)
-        except Exception as exc:
-            raise exc
+    train_step(model, train_seq, verbose, epochs, callbacks, save_path, val_seq)
+    save_model(model, save_path)
+    sys.exit()
 
+    for epoch in range(epochs):
+        vi = 0
+        stepnum = 0
+        for x,y in train_seq:
+            stepnum += 1
+            vx, vy = val_seq[vi]
+            vi += 1
+            if vi >= len(val_seq):
+                vi = 0
+            try:
+                print("Epoch %d / %d, step %d / %d" % (epoch, epochs, stepnum, len(train_seq)))
+                model.fit(x, y, validation_data=[vx, vy], epochs=1,
+                            verbose=verbose, batch_size=3)
+            except Exception as exc:
+                save_model(model, save_path)
+                raise exc
     save_model(model, save_path)
 
 def train_step(model, train_seq, verbose, epochs, callbacks, save_path, val_seq):

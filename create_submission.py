fb7e813dca51b0aa0584ed6892f08afb3d0d6f67
Murray Miron <mmiron@srllc.org>
2019-10-24 01:29:04 -0400

    Fix speed calculations in create_submission.py

--- create_submission.py
+++ create_submission.py
@@ -20,7 +20,7 @@ def rotate(x, y, angle):
     return np.matmul(rotation_matrix(theta), mat)
 
 def tr_x(x):
-    """Translated resized target image coordinates to input image coordinates"""
+    """Translate resized target image coordinates to input image coordinates"""
     x = x * flow.ORIG_IMSHAPE[0] / flow.IMSHAPE[0]
     return x
 
@@ -53,6 +53,9 @@ def get_coords(graph, cn=255, ce=128):
     return points, weights
 
 def get_weight_for(mask, graph, cp):
+    """Return the average speed, judged by pixel intensity and color,
+       for the given points.  Ret val should be multiplied by path length."""
+    max_speed = 29
     xs = cp[:,1]
     ys = cp[:,0]
 
@@ -61,9 +64,12 @@ def get_weight_for(mask, graph, cp):
         channels = mask[int(round(x))][int(round(y))]
         for i in range(1, mask.shape[-1]):
             # channel[0] * 3, channel[1] * 2, channel[2] * 1, etc.
-            weight += channels[i] * (flow.N_CLASSES - i)
-    if weight == 0:
-        weight += 1
+            if channels[i] > 1.0:
+                log.critical("expected range [0.0,1.0], got {}".format(channels[i]))
+            weight += (max_speed - flow.get_channel_speed(i)) * channels[i]
+        weight = weight / 3
+    if weight <= 0:
+        weight = 1
     assert weight > 0, "did not detect the color of a path for {}".format(graph.name)
 
     return weight
@@ -97,7 +103,12 @@ def graphs_to_wkt(masks, graphs, output_csv_path):
             pts = np.array(pts)
             xs = pts[:,1]
             ys = pts[:,0]
-
+            
+#            node, nodes = graph.node, graph.nodes()
+#            ps = np.array([node[i]['o'] for i in nodes])
+#            for (x, y) in (ps[:,1], ps[:,0]):
+#                pts.append([x,y])
+#            import pdb; pdb.set_trace()
 #            node, nodes = graph.node, graph.nodes()
 #            cps = [node[i]['o'] for i in nodes]
             # add nodes
@@ -129,7 +140,7 @@ def graphs_to_wkt(masks, graphs, output_csv_path):
                     p1,q1 = tr_x(xs[i-1]), tr_y(ys[i-1])
                     p2,q2 = tr_x(xs[i]), tr_y(ys[i])
                     # Euclidean distance
-                    #+= np.sqrt( (p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1) )
+                    euclid_distance = np.sqrt( (p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1) )
 
                 # construct linestring
                 linestring = "LINESTRING ({} {}".format(tr_x(xs[0]), tr_y(ys[0]))
@@ -141,10 +152,10 @@ def graphs_to_wkt(masks, graphs, output_csv_path):
                 # skip cycles starting and ending at the same node
                 if xs[0] == xs[-1] and ys[0] == ys[-1]:
                     continue
-                weights.append(get_weight_for(mask, graph, pts))
+                weights.append(get_weight_for(mask, graph, pts) * euclid_distance)
                 linestrings.append(linestring)
             else:
-                log.info("{}: unconnected point".format(chipname))
+                log.warning("{}: unconnected point {}".format(chipname, str(xs)))
 #        import pdb; pdb.set_trace()
         if len(linestrings) < 1:
             output_csv.write("{},".format(chipname))

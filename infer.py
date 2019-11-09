ec07f3dacbed3b0ffa523fbb4629b9d0e3b4d9d4
Murray Miron <mmiron@srllc.org>
2019-11-09 15:48:55 -0500

    Rewrite infer.py for xview2

--- infer.py
+++ infer.py
@@ -1,236 +1,60 @@
-import sys
-if 'snflow' in sys.modules:
-    flow = sys.modules['snflow']
-else:
-    import snflow as flow
-import numpy as np
-import cv2
-import skimage
-from skimage.morphology import skeletonize, medial_axis
-import matplotlib.pyplot as plt
-import sknw
-import keras
 import time
-import getch
+import random
+import tensorflow as tf
+import segmentation_models as sm
+import numpy as np
 import os
+import sys
 import skimage
-import copy
-#import cresi_skeletonize
+import train
+import flow
+import cv2
+import matplotlib.pyplot as plt
+from settings import *
 
-threshold_min = 0.03
-threshold_max = 1.0
-area_threshold=1
-connectivity=50
-THRESH_METHOD = cv2.ADAPTIVE_THRESH_MEAN_C
-THRESH_METHOD = cv2.THRESH_BINARY
+def get_submission_pair(mask):
+    assert mask.ndim == 3, f"expected 3 dimensions, got {mask.shape}"
+    dmg = np.zeros(TARGETSHAPE)
+    for i in range(0, mask.shape[-1]):
+        dmg[mask[:,:,i] != 0] = i
+    return mask,dmg
 
-def hysteresis_threshold(img):
-    low = 0.003
-    high = 0.35
-    edges = skimage.filters.sobel(img)
-    lowt = (edges > low).astype(int)
-    hight = (edges > high).astype(int)
-    hyst = skimage.filters.apply_hysteresis_threshold(edges, low, high)
-    return hight + hyst
+def infer(model, image):
+    assert image.ndim == 3, f"expected 3 dimensions, got {image.shape}"
+    mask = model.predict(np.expand_dims(image, axis=0))
+    return get_submission_pair(mask.squeeze())
 
-def dilate_and_erode(img):
-    kernel = np.ones((1,15))
-#    kernel = np.array([[1, 0, 1, 0, 1],
-#                       [0, 1, 9, 1, 0],
-#                       [1, 0, 1, 0, 1]], dtype = np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-    return img
+def show_random(model):
+    files = flow.get_test_files()
+    idx = random.randint(0,len(files)-1)
+    img = skimage.io.imread(files[idx])
 
-def prep_for_skeletonize(img):
-#    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-#    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#    img = dilate_and_erode(img)
-    """
-    maxval = np.max(img)
-    if maxval != np.nan and maxval != 0:
-        scale = 1 / maxval
-        img *= scale
-    """
-    if img.shape[-1] == 3:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    elif img.shape[-1] == 1:
-        img = img.squeeze()
-    else:
-        raise Exception("bad image shape: %s" % str(img.shape))
-    _, gray = cv2.threshold(img, threshold_min, threshold_max, THRESH_METHOD)
-#    _, gray = cv2.threshold(img, threshold_min, threshold_max, cv2.ADAPTIVE_THRESH_MEAN_C)
-#    img = skimage.filters.threshold_local(img, method='gaussian', block_size=25)
-#    img = hysteresis_threshold(img)
-#    filled = skimage.morphology.remove_small_holes(gray.astype(bool), connectivity=connectivity,
-#                                                area_threshold=area_threshold)
-#    filled = skimage.morphology.remove_small_objects(filled.astype(bool), connectivity=connectivity)
-    return gray
+    mask,dmg = infer(model, img)
+    mask2 = np.zeros(SAMPLESHAPE)
+    mask2[:,:,0:2] = mask[:,:,3:]
 
-def get_path_points(img):
-    # i = x * len + y
-    # x = (i - y) / len
-    # y = i - x * len
-    assert img.shape == (flow.IMSHAPE[0], flow.IMSHAPE[1])
-    paths = []
-    img = skeletonize(img)
-    img = img.ravel()
-    mark(img)
-    pts = np.where(img == 2)
-    seen = set()
-    ret = []
-    nbs = sknw.neighbors(img.shape)
-    ary = img.ravel()
-    L = img.shape[0]
-    for i in range(len(ary)):
-        if ary[i] == 0:
-            continue
-        x1 = i // L
-        y1 = i - x1
-        for d in nbs:
-            if ary[i+d] > 0:
-                x2 = (i+d) // L
-                y2 = (i+d) - x2
-                
-def infer_mask(model, image):
-    assert image.ndim == 3, "expected shape {}, got {}".format(flow.IMSHAPE, image.shape)
-    output = model.predict(np.expand_dims(image, axis=0))
-   # .reshape([1] + flow.IMSHAPE))
-    output = output.squeeze()
-    output = output[:,:,1:]
-    return flow.normalize(output)
+    fig = plt.figure()
+    fig.add_subplot(2,2,1)
+    plt.imshow(img)
+    plt.title("Input")
 
-def infer_roads(mask, chipname=''):
-    assert mask.ndim == 3, "expected shape {}, got {}".format(flow.IMSHAPE, mask.shape)
-    img = prep_for_skeletonize(mask.astype(np.float32))#.astype(np.uint8))
-    #skel = skeletonize(img)
-    skel = skeletonize(img)
-    skel = skimage.morphology.remove_small_holes(skel, connectivity=connectivity, area_threshold=area_threshold)
-    skel = skimage.morphology.remove_small_objects(skel, connectivity=connectivity * 3.0)
-    graph = sknw.build_sknw(skel)
-    graph.name = chipname
-    assert (np.max(img) == np.nan or np.max(img)) <= 1
-    assert (np.max(skel) == np.nan or np.max(skel)) <= 1
-    return graph, img, skel
+    fig.add_subplot(2,2,2)
+    plt.imshow(mask[:,:,:3])
+    plt.title("Mask channels 0-2")
 
+    fig.add_subplot(2,2,3)
+    plt.imshow(mask2)
+    plt.title("Mask channels 3-4")
 
-def infer(model, image, chipname=''):
-    assert image.ndim == 3, "expected shape {}, got {}".format(flow.IMSHAPE, image.shape)
-    mask = infer_mask(model, image)
-    graph, preproc, skel = infer_roads(mask, chipname)
-    return mask, graph, preproc, skel
+    fig.add_subplot(2,2,4)
+    plt.imshow(dmg.squeeze())
+    plt.title("Damage")
 
-def infer_and_show(model, image, filename):
-    tb = flow.TargetBundle()
-    chipid = os.path.basename(filename)#.replace("PS-RGB_", "").replace(".tif", "")
-    if isinstance(image, str):
-        image = flow.get_image(image)
-    mask = infer_mask(model, image)
-#    import pdb;pdb.set_trace()
-#    mask = infer_mask(model, image)
-    if mask.shape[-1] == 3:
-        graymask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-    else:
-        mask = mask.squeeze()
-#    mask = cresi_skeletonize.preprocess(mask, threshold_min)
-#    mask = flow.normalize(mask)
-    _, graymask = cv2.threshold(graymask, threshold_min, threshold_max, THRESH_METHOD)
-#    _, graymask = cv2.threshold(graymask, threshold_min, threshold_max, cv2.ADAPTIVE_THRESH_MEAN_C)
-    filled_mask = skimage.morphology.remove_small_holes(graymask.astype(bool), connectivity=connectivity, area_threshold=area_threshold)
-    filled_mask = skimage.morphology.remove_small_objects(filled_mask.astype(bool), connectivity=connectivity * 3.0)
-    _, graph, preproc, skel = infer(model, image)
-#    medial = prep_for_skeletonize(filled_mask.astype(np.float32))
-#    medial = medial_axis(medial)
-#    import pdb;pdb.set_trace()
-    for idx in range(1):
-        fig = plt.figure()
-        fig.add_subplot(2,4,1)
-        plt.axis('off')
-        plt.imshow(image)
-        plt.title("1. Satellite image (neural net input)")
-
-        fig.add_subplot(2,4,2)
-        plt.axis('off')
-        plt.imshow(mask)
-        plt.title("2. Mask (neural net output)")
-
-        fig.add_subplot(2,4,3)
-        plt.axis('off')
-        plt.imshow(filled_mask)
-        plt.title("3. Filled mask")
-
-        fig.add_subplot(2,4,4)
-        plt.axis('off')
-        plt.imshow(skeletonize(prep_for_skeletonize(mask)))
-        plt.title("4. Skeletonized mask")
-
-        fig.add_subplot(2,4,5)
-        plt.axis('off')
-        plt.imshow(skel)
-        plt.title("5. sknw skeleton")
-
-        fig.add_subplot(2,4,6)
-        plt.axis('off')
-        try:
-            plt.imshow(medial)
-        except Exception as exc:
-            pass
-        plt.title("6. Medial axis mask")
-
-        fig.add_subplot(2,4,7)
-        plt.axis('off')
-        plt.imshow(skel)
-        plt.title("7. Resulting graph")
-        for (s,e) in graph.edges():
-            ps = graph[s][e]['pts']
-            plt.plot(ps[:,1], ps[:,0], 'blue')
-    
-        node, nodes = graph.node, graph.nodes()
-        ps = np.array([node[i]['o'] for i in nodes])
-        try:
-            plt.plot(ps[:,1], ps[:,0], 'r+')
-        except IndexError as exc:
-            print("WARNING: IndexError: %s" % exc)
-
-        fig.add_subplot(2,4,8)
-        try:
-            plt.imshow(tb[chipid].image().squeeze())
-        except:
-            pass
-        plt.title("8. Ground truth")
-
-        plt.show()
-
-def do_all(model, loop=True):
-    import random
-    get_files = flow.get_test_filenames
-    while True:
-        if len(sys.argv) > 1:
-            if os.path.exists(sys.argv[1]):
-                path = sys.argv[1]
-            else:
-                path = flow.get_file(sys.argv[1])
-        else:
-            path = flow.get_file()
-        path = get_files()[random.randint(0,len(get_files()))]
-        if len(sys.argv) > 1:
-            path = flow.get_file(sys.argv[1])
-            sys.argv = [sys.argv[0]]
-        print(path)
-        image = flow.get_image(path)#resize(skimage.io.imread(path), flow.IMSHAPE).reshape(flow.IMSHAPE)
-        if not loop:
-            ret = infer(model, image) + (path,)
-            return ret
-        infer_and_show(model, image, path)
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            sys.exit()
+    plt.show()
 
 if __name__ == "__main__":
-    import logging
-    import train
     model = train.build_model()
-    train.load_weights(model)
-    logging.getLogger().setLevel(logging.ERROR)
-    do_all(model)
+    model = train.load_weights(model)
+    while True:
+        show_random(model)
+        time.sleep(1)

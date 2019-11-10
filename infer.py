d41aa7629d5c9e1d14f48be86247cc31b0f01b9c
Murray Miron <mmiron@srllc.org>
2019-11-09 20:56:46 -0500

    Fix inference

--- infer.py
+++ infer.py
@@ -12,21 +12,16 @@ import cv2
 import matplotlib.pyplot as plt
 from settings import *
 
-def get_submission_pair(mask):
-    """Generate the damage image from the output of the model (i.e. from the
-       predicted mask)"""
-    assert mask.ndim == 3, f"expected 3 dimensions, got {mask.shape}"
-    dmg = np.zeros(TARGETSHAPE)
-    for i in range(0, mask.shape[-1]):
-        dmg[mask[:,:,i] != 0] = i
-    return mask,dmg
 
-
-def infer(model, image):
+def infer(model, pre, post=None):
     """Do prediction, returning (mask_image, damage_image)"""
-    assert image.ndim == 3, f"expected 3 dimensions, got {image.shape}"
-    mask = model.predict(np.expand_dims(image, axis=0))
-    return get_submission_pair(mask.squeeze())
+    assert pre.ndim == 3, f"expected 3 dimensions, got {pre.shape}"
+    premask = model.predict(np.expand_dims(pre, axis=0))
+    if post is None:
+        return premask.squeeze()
+
+    postmask = model.predict(np.expand_dims(post, axis=0))
+    return premask.squeeze(), postmask.squeeze()
 
 
 def show_random(model):

188a880379d5919ac28d25abf49bdb4fd8e43413
Murray Miron <mmiron@srllc.org>
2019-11-09 23:59:24 -0500

    Return (h,w,1) solution images instead of (h,w,6) model outputs

--- infer.py
+++ infer.py
@@ -13,6 +13,14 @@ import matplotlib.pyplot as plt
 from settings import *
 
 
+def compress_channels(mask):
+    assert mask.shape == tuple(MASKSHAPE), f"expected {MASKSHAPE}, got {mask.shape}"
+    cmpr = np.zeros(TARGETSHAPE, dtype=np.uint8)
+    for i in range(mask.shape[-1] - 1):
+        cmpr[mask[:,:,i] > 0.01] = i
+    return cmpr.squeeze()
+
+
 def infer(model, pre, post=None):
     """Do prediction, returning (mask_image, damage_image)"""
     assert pre.ndim == 3, f"expected 3 dimensions, got {pre.shape}"
@@ -21,34 +29,37 @@ def infer(model, pre, post=None):
         return premask.squeeze()
 
     postmask = model.predict(np.expand_dims(post, axis=0))
-    return premask.squeeze(), postmask.squeeze()
+    return compress_channels(premask.squeeze()), compress_channels(postmask.squeeze())
 
 
 def show_random(model):
     """Choose a random test image and display the results of inference"""
-    files = flow.get_test_files()
+    files = list(flow.get_test_files())
     idx = random.randint(0,len(files)-1)
-    img = skimage.io.imread(files[idx])
+    preimg = skimage.io.imread(files[idx][0])
+    postimg = skimage.io.imread(files[idx][1])
 
-    mask,dmg = infer(model, img)
-    mask2 = np.zeros(SAMPLESHAPE)
-    mask2[:,:,0:2] = mask[:,:,3:]
+    mask,dmg = infer(model, preimg, postimg)
 
     fig = plt.figure()
-    fig.add_subplot(2,2,1)
-    plt.imshow(img)
-    plt.title("Input")
+    fig.add_subplot(3,3,1)
+    plt.imshow(preimg)
+    plt.title(files[idx][0])
 
-    fig.add_subplot(2,2,2)
-    plt.imshow(mask[:,:,:3])
+    fig.add_subplot(3,3,2)
+    plt.imshow(postimg)
+    plt.title(files[idx][1])
+
+    fig.add_subplot(3,3,3)
+    plt.imshow(mask,cmap='gray')
     plt.title("Mask channels 0-2")
 
-    fig.add_subplot(2,2,3)
-    plt.imshow(mask2)
-    plt.title("Mask channels 3-4")
+    fig.add_subplot(3,3,4)
+    plt.imshow(mask,cmap='gray')
+    plt.title("Mask channels 3-5")
 
-    fig.add_subplot(2,2,4)
-    plt.imshow(dmg.squeeze())
+    fig.add_subplot(3,3,5)
+    plt.imshow(dmg.squeeze(),cmap='gray')
     plt.title("Damage")
 
     plt.show()

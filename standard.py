3255a0761140fd2a78e10c27a8f81eb448eb6956
Murray Miron <mmiron@srllc.org>
2019-11-13 16:47:26 -0500

    Standard imports

---
+++ standard.py
@@ -0,0 +1,10 @@
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+from inspect import getsourcefile
+import plac
+import pandas as pd
+import numpy as np
+import re
+import logging
+import random

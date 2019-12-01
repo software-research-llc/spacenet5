95bb76354ddb107de71b234168ba8a75400319dc
Murray Miron <mmiron@srllc.org>
2019-12-01 18:28:10 -0500

    Try out a new loss

--- ordinal_loss.py
+++ ordinal_loss.py
@@ -15,4 +15,4 @@ def loss(y_true, y_pred):
     Ordinal categorical crossentropy
     """
     weights = K.cast(K.abs(K.argmax(y_true, axis=3) - K.argmax(y_pred, axis=3)), dtype='float32')
-    return K.clip((1.0 + weights) * losses.binary_crossentropy(y_true, y_pred), 0., 1.)
+    return K.clip((1.0 - weights) * losses.binary_crossentropy(y_true, y_pred), 0., 10.)

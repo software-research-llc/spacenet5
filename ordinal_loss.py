69f06e9225c1caaacf4fd6ca16afd4c984a6711f
Murray Miron <mmiron@srllc.org>
2019-12-21 14:36:15 -0500

    Force ordinal_loss to work

--- ordinal_loss.py
+++ ordinal_loss.py
@@ -3,15 +3,31 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras import losses
 
+
+#from keras import backend as K
+#from keras import losses
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 def loss(y_true, y_pred):
-    """
-    Ordinal categorical crossentropy
-    """
-    one = K.argmax(y_true, axis=1)
-    two = K.argmax(y_pred, axis=1)
-    three = K.int_shape(y_pred)[1]
-    if one is not None and two is not None and three is not None:
-        weights = K.cast(K.abs(one - two)/(three - 1), dtype='float32')
-    else:
-        weights = 0
+    if K.int_shape(y_pred)[1] is None:
+        logger.debug("None value in K.int_shape")
+        return losses.categorical_crossentropy(y_true, y_pred)
+
+    weights = K.cast(K.abs(K.argmax(y_true, axis=1) - K.argmax(y_pred, axis=1))/(K.int_shape(y_pred)[1] - 1), dtype='float32')
     return (1.0 + weights) * losses.categorical_crossentropy(y_true, y_pred)
+
+#def loss(y_true, y_pred):
+#    """
+#    Ordinal categorical crossentropy
+#    """
+#    one = K.argmax(y_true, axis=1)
+#    two = K.argmax(y_pred, axis=1)
+#    three = K.int_shape(y_pred)[1]
+#    if one is not None and two is not None and three is not None:
+#        weights = K.cast(K.abs(one - two)/(three - 1), dtype='float32')
+#    else:
+#        weights = 0
+#    return (1.0 + weights) * losses.categorical_crossentropy(y_true, y_pred)

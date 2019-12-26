767b8d87dea8262f00a01810b6b941285670eed7
Murray Miron <mmiron@srllc.org>
2019-12-26 17:27:08 -0500

    Cleanup and comment

--- unet.py
+++ unet.py
@@ -1,27 +1,21 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-from keras.layers import Flatten, Reshape, MaxPooling2D, Dropout, BatchNormalization, Conv2D, Activation, Conv2DTranspose, Concatenate, concatenate, Conv3D, Cropping2D
-from keras.models import Model
-import keras
 import tensorflow as tf
-import flow as flow
 import tensorflow.keras.layers as L
-from tensorflow_examples.models.pix2pix import pix2pix
-from settings import *
 import settings as S
-import tensorflow_datasets as tfds
-tfds.disable_progress_bar()
-import tensorflow as tf
-
-from IPython.display import clear_output
-import matplotlib.pyplot as plt
 
 
-DOWN = -1
-UP = 1
-OUT = 0
-OUTPUT_CHANNELS = S.N_CLASSES
 
 class MotokimuraUnet():
+    """
+    Slightly downsized Unet design by Motokimura (winner of SpaceNet challenge).
+
+    Model for single or multiclass semantic segmentation.
+
+    With a GeForce GTX 1080 Ti, trains in about 24 hours with model weight files of 82MB.
+
+    Xview2 scores:
+        Multiclass - 0.77 localization, 0.3 damage classification (0.41 overall).
+        Single class - 0.81 localization, 0.0 damage classification (0.25 overall).
+    """
     def __init__(self, *args, **kwargs):
         if 'classes' not in kwargs:
             raise KeyError("pass number of classes as classes=N")
@@ -121,24 +115,6 @@ class MotokimuraUnet():
 
         self.model = tf.keras.models.Model(inputs=[inp], outputs=[d0])
 
-    def convert_to_damage_classifier(self):
-        L = tf.keras.layers
-
-        #x = self.model.output
-        x = self.model.get_layer("encoder_out").output
-        #x = self.model.output
-        x = L.Flatten()(x)
-        x = L.Dense(64, activation='relu')(x)
-        x = L.Dropout(0.1)(x)
-        x = L.Dense(128, activation='relu')(x)
-        x = L.Dropout(0.1)(x)
-        x = L.Dense(4, activation='softmax')(x)
-
-        m = tf.keras.models.Model(inputs=self.model.input, outputs=[x])
-        self.model = m
-
-        return self
-
     def compile(self, *args, **kwargs):
         return self.model.compile(*args, **kwargs)
 
@@ -153,7 +129,15 @@ class MotokimuraUnet():
 
 
 class SegmentationModel(MotokimuraUnet):
+    """
+    MotokimuraUnet modified to use a MobileNetV2 encoder and an extra deconv layer.
+
+    Model for single class or multiclass semantic segmentation.
+
+    With a GeForce GTX 1080 Ti, trains in about 24 hours with model weight files of 71MB.
+    """
     def __init__(self, *args, **kwargs):
+        # The encoder (feature extractor)
         self.mobilenetv2 = tf.keras.applications.mobilenet_v2.MobileNetV2(input_shape=S.INPUTSHAPE,
                                                                           weights=None,
                                                                           include_top=False)
@@ -163,6 +147,8 @@ class SegmentationModel(MotokimuraUnet):
         damage = S.DAMAGE
         s = self
         factor = 5
+
+        # The decoder layers
         s.dc10 = L.Conv2DTranspose(2 ** (factor+4), kernel_size=(4,4), strides=2, padding='same',
                 use_bias=True if damage else False)
         #s.dc9 = L.Conv2D(2 ** (factor+4), kernel_size=(3,3), strides=1, padding='same',
@@ -185,6 +171,7 @@ class SegmentationModel(MotokimuraUnet):
                 use_bias=True if damage else False)
         s.dc0 = L.Conv2D(kwargs['classes'], kernel_size=(3,3), strides=1, padding='same', name='decoder_out')
 
+        # BatchNormalization for every layer
         s.bnd9 = L.BatchNormalization()
         s.bnd8 = L.BatchNormalization()
         s.bnd7 = L.BatchNormalization()
@@ -195,8 +182,10 @@ class SegmentationModel(MotokimuraUnet):
         s.bnd2 = L.BatchNormalization()
         s.bnd1 = L.BatchNormalization()
 
+        # Final MobileNetV2 output
         mobilenet_out = self.mobilenetv2.get_layer("out_relu").output # 32x32
-
+        
+        # Skip connections
         e10 = self.mobilenetv2.get_layer("block_15_project_BN").output # 32x32
         e8 = self.mobilenetv2.get_layer("block_12_project_BN").output # 64x64
         e6 = self.mobilenetv2.get_layer("block_5_project_BN").output # 128x128
@@ -204,7 +193,7 @@ class SegmentationModel(MotokimuraUnet):
         e2 = self.mobilenetv2.get_layer("expanded_conv_project_BN").output # 512x512
         e0 = self.mobilenetv2.get_layer("Conv1_relu").output # 512x512
 
-
+        # Put it all together
         d10 = L.Activation('relu')(s.bnd9(s.dc10(L.Concatenate()([mobilenet_out,e10]))))
         #d9 = L.Activation('relu')(s.bnd9(s.dc9(d10)))
         d8 = L.Activation('relu')(s.bnd8(s.dc8(L.Concatenate()([e8,d10]))))
@@ -218,271 +207,3 @@ class SegmentationModel(MotokimuraUnet):
         d0 = s.dc0(d1)#L.Concatenate()([e0,d1]))
 
         self.model = tf.keras.models.Model(inputs=self.mobilenetv2.inputs, outputs=[d0])
-
-def Discriminator():
-  initializer = tf.random_normal_initializer(0., 0.02)
-
-  inp = tf.keras.layers.Input(shape=[None, None, 3], name='input_image')
-  tar = tf.keras.layers.Input(shape=[None, None, 3], name='target_image')
-
-  x = tf.keras.layers.concatenate([inp, tar]) # (bs, 256, 256, channels*2)
-
-  down1 = downsample(64, 4, False)(x) # (bs, 128, 128, 64)
-  down2 = downsample(128, 4)(down1) # (bs, 64, 64, 128)
-  down3 = downsample(256, 4)(down2) # (bs, 32, 32, 256)
-
-  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) # (bs, 34, 34, 256)
-  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
-                                kernel_initializer=initializer,
-                                use_bias=False)(zero_pad1) # (bs, 31, 31, 512)
-
-  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
-
-  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
-
-  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu) # (bs, 33, 33, 512)
-
-  last = tf.keras.layers.Conv2D(1, 4, strides=1,
-                                kernel_initializer=initializer)(zero_pad2) # (bs, 30, 30, 1)
-
-  return tf.keras.Model(inputs=[inp, tar], outputs=last)
-
-def downsample(filters, size, apply_batchnorm=True):
-  initializer = tf.random_normal_initializer(0., 0.02)
-
-  result = tf.keras.Sequential()
-  result.add(
-      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
-                             kernel_initializer=initializer, use_bias=False))
-
-  if apply_batchnorm:
-    result.add(tf.keras.layers.BatchNormalization())
-
-  result.add(tf.keras.layers.LeakyReLU())
-
-  return result
-
-def upsample(filters, size, apply_dropout=False):
-  initializer = tf.random_normal_initializer(0., 0.02)
-
-  result = tf.keras.Sequential()
-  result.add(
-    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
-                                    padding='same',
-                                    kernel_initializer=initializer,
-                                    use_bias=False))
-
-  result.add(tf.keras.layers.BatchNormalization())
-
-  if apply_dropout:
-      result.add(tf.keras.layers.Dropout(0.5))
-
-  result.add(tf.keras.layers.ReLU())
-
-  return result
-
-def gan():
-  down_stack = [
-    downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
-    downsample(128, 4), # (bs, 64, 64, 128)
-    downsample(256, 4), # (bs, 32, 32, 256)
-    downsample(512, 4), # (bs, 16, 16, 512)
-    downsample(512, 4), # (bs, 8, 8, 512)
-    downsample(512, 4), # (bs, 4, 4, 512)
-    downsample(512, 4), # (bs, 2, 2, 512)
-    downsample(512, 4), # (bs, 1, 1, 512)
-  ]
-
-  up_stack = [
-    upsample(512, 4, apply_dropout=True), # (bs, 2, 2, 1024)
-    upsample(512, 4, apply_dropout=True), # (bs, 4, 4, 1024)
-    upsample(512, 4, apply_dropout=True), # (bs, 8, 8, 1024)
-    upsample(512, 4), # (bs, 16, 16, 1024)
-    upsample(256, 4), # (bs, 32, 32, 512)
-    upsample(128, 4), # (bs, 64, 64, 256)
-    upsample(64, 4), # (bs, 128, 128, 128)
-  ]
-
-  initializer = tf.random_normal_initializer(0., 0.02)
-  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
-                                         strides=2,
-                                         padding='same',
-                                         kernel_initializer=initializer,
-                                         activation='tanh') # (bs, 256, 256, 3)
-
-  concat = tf.keras.layers.Concatenate()
-
-  inputs = tf.keras.layers.Input(shape=[None,None,3])
-  x = inputs
-
-  # Downsampling through the model
-  skips = []
-  for down in down_stack:
-    x = down(x)
-    skips.append(x)
-
-  skips = reversed(skips[:-1])
-
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    x = concat([x, skip])
-
-  x = last(x)
-
-  return tf.keras.Model(inputs=inputs, outputs=x)
-
-def conv3x3_relu_block(inp, n_filters, kernel_size=3, direction=DOWN):
-    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), activation='relu')(inp)
-    y = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), activation='relu')(x)
-
-    if direction is DOWN:
-        x = MaxPooling2D((1, 1), strides=(2, 2))(y)
-    elif direction is UP:
-        x = Conv2DTranspose(n_filters, kernel_size=(kernel_size, kernel_size),
-                            strides=(2, 2), activation='relu')(y)
-#        x = Conv2D(n_filters, kernel_size=(kernel_size, kernel_size), activation='relu')(x)
-    else:
-        x = Conv2D(n_filters, kernel_size=(1,1))(y)
-
-    return x, y
-
-def build(inp):
-    base_filters = 64
-    n_classes = 4
-    x1,y1 = conv3x3_relu_block(inp, n_filters=base_filters)
-    x2,y2 = conv3x3_relu_block(x1, n_filters=base_filters * 2)
-    x3,y3 = conv3x3_relu_block(x2, n_filters=base_filters * 2 ** 2)
-    x4,y4 = conv3x3_relu_block(x3, n_filters=base_filters * 2 ** 3)
-
-    x5,y5 = conv3x3_relu_block(x4, kernel_size=3, n_filters=base_filters * 2 ** 4, direction=UP)
-    x5 = Cropping2D((15,15))(x5)
-
-    x = concatenate([x5, y5])
-    x, _ = conv3x3_relu_block(x, kernel_size=2, n_filters=base_filters * 2 ** 3, direction=UP)
-    y3 = concatenate([x3, y3])
-    y2 = conv3x3_relu_block(y3, kernel_size=2, n_filters=base_filters * 2 ** 2, direction=UP)
-    y2 = concatenate([x2,y2])
-    y1 = conv3x3_relu_block(y2, kernel_size=2, n_filters=base_filters * 2, direction=UP)
-    y1 = concatenate([x1,y1])
-
-    x = conv3x3_relu_block(y1, n_filters=n_classes, direction=OUT)
-    return keras.models.Model(inputs=[inp], outputs=[x])
-
-def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
-    # first layer
-    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size),  kernel_initializer="he_normal",
-#               kernel_regularizer=keras.regularizers.l1_l2(0.01),
-               padding="same")(input_tensor)
-    if batchnorm:
-        x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    # second layer
-    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
-#               kernel_regularizer=keras.regularizers.l1_l2(0.01),
-               padding="same")(x)
-    if batchnorm:
-        x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    return x
-
-def get_unet(input_img, n_filters=16, dropout=0.5, batchnorm=True):
-    inp = keras.layers.Input(input_img)
-    c1 = conv2d_block(inp, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
-    p1 = MaxPooling2D((2, 2)) (c1)
-    p1 = Dropout(dropout / 2)(p1)
-
-    c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
-    p2 = MaxPooling2D((2, 2)) (c2)
-    p2 = Dropout(dropout)(p2)
-
-    c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
-    p3 = MaxPooling2D((2, 2)) (c3)
-    p3 = Dropout(dropout)(p3)
-
-    c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
-    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-    p4 = Dropout(dropout)(p4)
-    
-    c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3, batchnorm=batchnorm)
-   
-    u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
-    u6 = concatenate([u6, c4])
-    u6 = Dropout(dropout)(u6)
-    c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
-
-    u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
-    u7 = concatenate([u7, c3])
-    u7 = Dropout(dropout)(u7)
-    c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
-
-    u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
-    u8 = concatenate([u8, c2])
-    u8 = Dropout(dropout)(u8)
-    c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
-
-    u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
-    u9 = concatenate([u9, c1], axis=3)
-    u9 = Dropout(dropout)(u9)
-    c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
-
-    #outputs = Conv2D(flow.N_CLASSES, (1, 1), activation=flow.LAST_LAYER_ACTIVATION) (c9)
-    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
-    model = Model(inputs=[inp], outputs=[outputs])
-    return model
-
-def build_google_unet():
-    output_channels = 1
-    base_model = tf.keras.applications.MobileNetV2(input_shape=INPUTSHAPE, include_top=False)
-    #base_model.trainable = False
-
-    # Use the activations of these layers
-    layer_names = [
-        'block_1_expand_relu',   # 64x64
-        'block_3_expand_relu',   # 32x32
-        'block_6_expand_relu',   # 16x16
-        'block_13_expand_relu',  # 8x8
-        'block_16_project',      # 4x4
-    ]
-    layers = [base_model.get_layer(name).output for name in layer_names]
-
-    # Create the feature extraction model
-    down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
-
-#    down_stack.trainable = False
-
-    up_stack = [
-        pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-        pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-        pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-        pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-    ]
-
-    # This is the last layer of the model
-    last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same', activation='sigmoid')  #64x64 -> 128x128
-
-    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-    x = inputs
-
-    # Downsampling through the model
-    skips = down_stack(x)
-    x = skips[-1]
-    skips = reversed(skips[:-1])
-    
-    # Upsampling and establishing the skip connections
-    i = 0
-    for up, skip in zip(up_stack, skips):
-        try:
-            x = up(x)
-            concat = tf.keras.layers.Concatenate()
-            x = concat([x, skip])
-    
-        except Exception as exc:
-            print("on iteration %d" % i)
-            raise exc
-        i += 1
-    x = last(x)
-    return tf.keras.Model(inputs=inputs, outputs=x)
-

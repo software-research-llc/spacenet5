ed17bd1bed78f20dfe80f49ac3cb1df973bc46d4
Murray Miron <mmiron@srllc.org>
2019-12-26 19:26:45 -0500

    Show raw pixel counts as well as percentages

--- scansolution.py
+++ scansolution.py
@@ -1,3 +1,7 @@
+"""
+Scan solution .png files and return a proportional count of
+pixel values.
+"""
 import skimage
 import numpy as np
 import os
@@ -5,22 +9,25 @@ import sys
 import tqdm
 import random
 from collections import Counter
+import settings as S
+
+# fix invalid pixel values in a really dumb way
+ALTER_SOLUTION = False
 
 counts = Counter()
 totals = Counter()
 areas = Counter()
-filecount = 0
+total = 0
 
 for file in tqdm.tqdm(os.listdir("solution")):
     img = skimage.io.imread("solution/{}".format(file))
-    filecount += 1
     for i in range(6):
-        totals = len(img[img>0])
         c = len(img[img==i])
-        if totals > 0:
-            c /= totals
         counts[i] += c
-    """
+        total += c
+
+    if not ALTER_SOLUTION:
+        continue
     if len(img[img > 4]) > 0:
         img[img > 4] = random.randint(2,3)
         print("Corrected {}".format(file))
@@ -29,7 +36,7 @@ for file in tqdm.tqdm(os.listdir("solution")):
         img[img < 0] = 0
         print("Corrected {}".format(file))
         skimage.io.imsave("solution/{}".format(file), img, check_contrast=False)
-    """
-print('Counts:')
+
+print('Pixels by class:')
 for i in range(6):
-    print("{}: {}".format(i, counts[i]))
+    print("  {:13s}: {:.5f}% ({})".format(str(S.CLASSES[i]), counts[i] / total, counts[i]))

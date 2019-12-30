79b2478ec7ab145633ea19bfd395f4723cf25b15
Murray Miron <mmiron@srllc.org>
2019-12-30 06:42:00 -0500

    Implement accurate damage classification score

--- score.py
+++ score.py
@@ -12,6 +12,51 @@ import test
 logger = logging.getLogger(__name__)
 
 
+# Used for running_damage_f1_score
+global_tp = [0.] * S.N_CLASSES
+global_fp = [0.] * S.N_CLASSES
+global_fn = [0.] * S.N_CLASSES
+NUM_SAMPLES = 0
+SAMPLES_SEEN = 0
+# also used for running_damage_f1_score
+CLASS_1 = [0.] * S.N_CLASSES
+CLASS_1[1] = 1.
+CLASS_1 = CLASS_1 * S.MASKSHAPE[0] * S.MASKSHAPE[1] * S.BATCH_SIZE
+CLASS_1 = tf.constant(tf.reshape(CLASS_1, [S.BATCH_SIZE, S.MASKSHAPE[0] * S.MASKSHAPE[1], S.N_CLASSES]))
+CLASS_2 = [0.] * S.N_CLASSES
+CLASS_2[2] = 1.
+CLASS_2 = CLASS_2 * S.MASKSHAPE[0] * S.MASKSHAPE[1] * S.BATCH_SIZE
+CLASS_2 = tf.constant(tf.reshape(CLASS_2, [S.BATCH_SIZE, S.MASKSHAPE[0] * S.MASKSHAPE[1], S.N_CLASSES]))
+CLASS_3 = [0.] * S.N_CLASSES
+CLASS_3[3] = 1.
+CLASS_3 = CLASS_3 * S.MASKSHAPE[0] * S.MASKSHAPE[1] * S.BATCH_SIZE
+CLASS_3 = tf.constant(tf.reshape(CLASS_3, [S.BATCH_SIZE, S.MASKSHAPE[0] * S.MASKSHAPE[1], S.N_CLASSES]))
+CLASS_4 = [0.] * S.N_CLASSES
+CLASS_4[4] = 1.
+CLASS_4 = CLASS_4 * S.MASKSHAPE[0] * S.MASKSHAPE[1] * S.BATCH_SIZE
+CLASS_4 = tf.constant(tf.reshape(CLASS_4, [S.BATCH_SIZE, S.MASKSHAPE[0] * S.MASKSHAPE[1], S.N_CLASSES]))
+
+
+def initialize_f1(num_samples: "Number of samples in dataset (e.g. `len(dataflow)`"):
+    """
+    To get around the fact that metrics are only passed a BATCH_SIZE number of samples
+    per call (and to avoid RAM issues), we keep a running total of true positives, false
+    positives, and false negatives that we update every time running_damage_f1_score() is
+    called.  When that function is called as many times as there are samples in the dataset,
+    it resets the tallies.
+
+    This function initializes/resets those numbers.
+    """
+    global SAMPLES_SEEN
+    global NUM_SAMPLES
+    global global_tp, global_fp, global_fn
+    NUM_SAMPLES = num_samples
+    SAMPLES_SEEN = 0
+    for i in range(1,5):
+        global_tp[i] = 0
+        global_fp[i] = 0
+        global_fn[i] = 0
+
 
 def _f1_stats(tp, fp, fn):
     prec = tp / (tp + fp)
@@ -154,24 +199,111 @@ def nonlogical_f1_score(y_true, y_pred):
 def damage_f1_score(y_true, y_pred):
     gt = y_true
     scores = {}
+
     for i in range(1, 5):
+        # ignore all classes but the current one (multiply by 0s everywhere but the current class index)
         pixels = [0.] * S.N_CLASSES
         pixels[i] = 1.
         pixels = pixels * S.MASKSHAPE[0] * S.MASKSHAPE[1] * S.BATCH_SIZE
         pixels = tf.reshape(pixels, [S.BATCH_SIZE, S.MASKSHAPE[0] * S.MASKSHAPE[1], S.N_CLASSES])
         pr = y_pred * pixels
 
+        # get true positives, false positives, and false negatives
         tp = tf.reduce_sum(gt * pr)
         fp = tf.reduce_sum(tf.clip_by_value(pr - gt, 0, 1))
         fn = tf.reduce_sum(tf.clip_by_value(gt - pr, 0, 1))
 
+        # get precision and recall
         prec = tp / (tp + fp + 1e-6)
         rec = tp / (tp + fn + 1e-6)
 
+        # get f1-score for this class
         score = 2 * tf.math.multiply_no_nan(prec, rec) / (prec + rec + 1e-6)
         scores[i] = score
 
+    # use the contest formula to calculate damage score and return it
     df1 = 4 / np.sum([1 / (scores[i] + 1e-6) for i in range(1,5)])
+    return df1
+
+
+@tf.function
+def _isolate_class_1(y_pred):
+    pr = y_pred * CLASS_1
+
+    return pr
+
+@tf.function
+def _isolate_class_2(y_pred):
+    pr = y_pred * CLASS_2 
+
+    return pr
+
+@tf.function
+def _isolate_class_3(y_pred):
+    pr = y_pred * CLASS_3
+
+    return pr
+
+@tf.function
+def _isolate_class_4(y_pred):
+    pr = y_pred * CLASS_4
+
+    return pr
+
+
+def running_damage_f1_score(y_true, y_pred):
+    """
+    To get around the fact that metrics are only passed a BATCH_SIZE number of samples
+    per call (and to avoid RAM issues), we keep a running total of true positives, false
+    positives, and false negatives that we update every time running_damage_f1_score() is
+    called.  When this function is called as many times as there are samples in the dataset,
+    it resets the tallies.
+
+    Used to validate damage classification scores when this file is run as a stand alone script.
+    """
+    global global_tp, global_fp, global_fn, SAMPLES_SEEN, NUM_SAMPLES
+    y_true = tf.cast(y_true, tf.float32)
+    scores = {}
+
+    for i in range(1, 5):
+        # zero out everything but the current iteration's class of interest
+        if i == 1:
+            pr = _isolate_class_1(y_pred)
+            gt = _isolate_class_1(y_true)
+        elif i == 2:
+            pr = _isolate_class_2(y_pred)
+            gt = _isolate_class_2(y_true)
+        elif i == 3:
+            pr = _isolate_class_3(y_pred)
+            gt = _isolate_class_3(y_true)
+        elif i == 4:
+            pr = _isolate_class_4(y_pred)
+            gt = _isolate_class_4(y_true)
+
+        # standard f1-score variables
+        tp = tf.reduce_sum(gt * pr)
+        fp = tf.reduce_sum(tf.clip_by_value(pr - gt, 0, 1))
+        fn = tf.reduce_sum(tf.clip_by_value(gt - pr, 0, 1))
+
+        # update global tally of this damage class' counts
+        global_tp[i] += tp
+        global_fp[i] += fp
+        global_fn[i] += fn
+       
+        # use the global counts to calculate this damage class' F1-score
+        prec = global_tp[i] / (global_tp[i] + global_fp[i] + 1e-8)
+        rec = global_tp[i] / (global_tp[i] + global_fn[i] + 1e-8)
+
+        score = 2 * prec * rec / (prec + rec + 1e-8)
+        scores[i] = score
+
+    # reset values when we've been called as many times as there are samples
+    SAMPLES_SEEN += 1
+    if SAMPLES_SEEN >= NUM_SAMPLES:
+        initialize_f1(NUM_SAMPLES)
+
+    # xView2 contest formula (harmonic mean of all 4 damage class F1-scores)
+    df1 = 4 / np.sum([1 / (scores[i] + 1e-8) for i in range(1,5)])
     return df1
 
 
@@ -179,18 +311,22 @@ if __name__ == '__main__':
     import infer
     import math
     import train
-    model = train.build_model()
+    model = train.build_deeplab_model(classes=6, damage=True, train=False)
     S.BATCH_SIZE = 1
-    model = train.load_weights(model)
-    df = flow.Dataflow(files=flow.get_validation_files(), batch_size=1, shuffle=False)
+    model = train.load_weights(model, S.MODELSTRING)
+    df = flow.DamagedDataflow(files=flow.get_validation_files(), batch_size=1, shuffle=True, return_postmask=True, return_stacked=True)
     totals = 0#[0,0,0]
     num = 1
     pbar = tqdm.tqdm(df, desc="Scoring")
-    for x,y_ in pbar:
+    initialize_f1(len(df) * 2)
+
+    for x, y_ in pbar:
         pred = model.predict(x)#np.expand_dims(x[j], axis=0))
-        y_pred = infer.convert_prediction(pred)
-        y_true = infer.convert_prediction(y_)
-        scores = f1_score(y_true, y_pred)#sklearn.metrics.f1_score(y_true.astype(int), y_pred.astype(int), average='macro')
-        totals += scores
-        pbar.set_description("%f" % (totals / num))
+        #y_pred = infer.convert_prediction(pred)
+        #y_true = infer.convert_prediction(y_)
         num += 1
+        scores = running_damage_f1_score(y_, pred)#sklearn.metrics.f1_score(y_true.astype(int), y_pred.astype(int), average='macro')
+        pbar.set_description("%f" % (scores))#/ (num / 50)))
+    print("{:7s}{:10s}{:10s}{:10s}".format("Class", "True Pos", "False Pos", "False Neg"))
+    for i in range(1,5):
+        print("{:7s}{:10.10s}{:10.10s}{:10.10s}".format(str(i), str(global_tp[i].numpy()), str(global_fp[i].numpy()), str(global_fn[i].numpy())))
